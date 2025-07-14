b3e32e97bbf1b900fed1faee515e21ca3ec083f1
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-14 18:34:29 +0330

    feat: Add error logging to query client defaultOptions

--- src/providers/QueryClientProvider.tsx
+++ src/providers/QueryClientProvider.tsx
@@ -1,7 +1,14 @@
-import { QueryClientProvider as Provider } from "@tanstack/react-query";
+import { QueryClientProvider as Provider, type QueryClientConfig } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const QUERY_CLIENT_OPTIONS: QueryClientConfig = {
+  defaultOptions: {
+    mutations: {
+      onError: (error) => console.error(error),
+    },
+  },
+};
+const queryClient = new QueryClient(QUERY_CLIENT_OPTIONS);
 
 export const QueryClientProvider = ({ children }: FragmentProps) => {
   return (
