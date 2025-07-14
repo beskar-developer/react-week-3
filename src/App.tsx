f6d0b8544eeb91939386eb1050e2f2b24da42aa5
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-14 18:33:13 +0330

    feat: Add Movie routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { useInitRefreshTokenMutation } from "@/api/Authentication/mutations";
-
 const App = () => {
-  useInitRefreshTokenMutation();
-
   return (
     <div className={twMerge("app-container dark:text-white", "p-0")}>
       <RouterProvider />
---
+++ src/constants/Movie/router.ts
@@ -0,0 +1,6 @@
+export const ROUTES = Object.freeze({
+  ROOT_PATH: "/movie",
+  LIST_PATH: "list",
+  DETAILS_PATH: "details/:id",
+  LAYOUT_NAME: "Dashboard",
+});
