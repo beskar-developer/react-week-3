01aca1d79cf32a4705e4552ea44b2f5d494e53f7
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-15 22:01:17 +0330

    feat: Config QueryClient for better errors

--- src/providers/QueryClientProvider.tsx
+++ src/providers/QueryClientProvider.tsx
@@ -1,4 +1,7 @@
-import { QueryClientProvider as Provider, type QueryClientConfig } from "@tanstack/react-query";
+import { QueryClientProvider as Provider } from "@tanstack/react-query";
+
+import { ONE_MINUTE } from "@shared-vendor/constants";
+
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const QUERY_CLIENT_OPTIONS: QueryClientConfig = {
@@ -6,7 +9,13 @@ const QUERY_CLIENT_OPTIONS: QueryClientConfig = {
     mutations: {
       onError: (error) => console.error(error),
     },
+    queries: {
+      staleTime: 10 * ONE_MINUTE,
+    },
   },
+  queryCache: new QueryCache({
+    onError: (error) => console.error(error),
+  }),
 };
 const queryClient = new QueryClient(QUERY_CLIENT_OPTIONS);
 
