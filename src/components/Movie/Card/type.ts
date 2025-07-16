5ac9480de181af06dca0fb069beb266419f58428
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-16 11:30:11 +0330

    fix: Fix typeof IMovieCard

--- src/components/Movie/Card/type.ts
+++ src/components/Movie/Card/type.ts
@@ -1,6 +1,6 @@
 import type { Movie } from "@/api/Movie/type";
 
-export type IMovieCard = Movie;
+export type IMovieCard = Omit<Movie, "genreIds">;
 
 export interface IMovieCardRow {
   label?: string;
