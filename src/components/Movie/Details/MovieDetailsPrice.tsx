6c7bfab92a6be588462e65c700168de54332dd87
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-17 13:03:39 +0330

    feat: Add default value for MovieDetailsPrice

--- src/components/Movie/Details/MovieDetailsPrice.tsx
+++ src/components/Movie/Details/MovieDetailsPrice.tsx
@@ -6,10 +6,12 @@ interface IMovieDetailsPrice {
 }
 
 export const MovieDetailsPrice = ({ label, value }: IMovieDetailsPrice) => {
+  const normalizedValue = value ? convertNumberToLocaleString(value) : "نامشخص";
+
   return (
     <MovieCardRow label={label}>
       <span className="flex items-center gap-1">
-        {convertNumberToLocaleString(value)}
+        {normalizedValue}
 
         <AiFillDollarCircle className="text-success-500" />
       </span>
