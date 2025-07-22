8512211861bd7c8224e62a35e182e861f6679058
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-22 18:58:16 +0330

    feat: Add CATEGORY_TYPE_OPTIONS constant

--- src/components/Finance/Category/Chip/FinanceCategoryChip.tsx
+++ src/components/Finance/Category/Chip/FinanceCategoryChip.tsx
@@ -1,13 +1,19 @@
 import type { Category } from "@/types/Finance";
 
+import { CATEGORY_TYPE_OPTIONS } from "@/constants/Finance";
+
 const CHIP_COLOR_MAP = {
   INCOME: "success",
   EXPENSE: "error",
 } as const;
-const CHIP_LABEL_MAP = {
-  INCOME: "درآمد",
-  EXPENSE: "هزینه",
-};
+
+const CHIP_LABEL_MAP = CATEGORY_TYPE_OPTIONS.reduce(
+  (labelMap, { value, label }) => ({
+    ...labelMap,
+    [value]: label,
+  }),
+  {} as Record<Category["type"], string>,
+);
 
 export const FinanceCategoryChip = ({ type }: Pick<Category, "type">) => {
   return <Chip color={CHIP_COLOR_MAP[type]}>{CHIP_LABEL_MAP[type]}</Chip>;
---
+++ src/constants/Finance/options.ts
@@ -0,0 +1,10 @@
+export const CATEGORY_TYPE_OPTIONS = [
+  {
+    label: "درآمد",
+    value: "INCOME" as const,
+  },
+  {
+    label: "هزینه",
+    value: "EXPENSE" as const,
+  },
+];
