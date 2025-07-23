4899bdb6faea3e8e39df3341e0224b52f452b3a8
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-24 01:46:33 +0330

    feat: Add Loading to FinanceReportContent

--- src/components/Finance/Report/Content/FinanceReportContent.tsx
+++ src/components/Finance/Report/Content/FinanceReportContent.tsx
@@ -1,9 +1,15 @@
-export const FinanceReportContent = () => {
-  return (
-    <div className="flex h-full flex-col gap-6 overflow-auto pb-4">
-      <FinanceReportStatistics />
+import { useTransactionReportQuery } from "@/api/Finance/query";
 
-      <FinanceReportCharts />
+export const FinanceReportContent = () => {
+  const { isFetching } = useTransactionReportQuery();
+
+  return (
+    <div className={twMerge("flex h-full flex-col gap-6 overflow-auto pb-4", isFetching && "justify-center")}>
+      <LoadingContainer message="در حال دریافت گزارشات..." loading={isFetching}>
+        <FinanceReportStatistics />
+
+        <FinanceReportCharts />
+      </LoadingContainer>
     </div>
   );
 };
